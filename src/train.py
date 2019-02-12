2f757257a8247d42d8f623d1645f8fe79ac96581
Natalie Garza <n_g137@txstate.edu>
2019-02-11 22:03:50 -0600

    Code now runs and generates something...
    Fixed key error and did some clean up of old test code. Will need to tinker with network since the results are not very good right now.

--- src/train.py
+++ src/train.py
@@ -4,15 +4,16 @@
 
 # Train recurrent neural network (RNN) using long short-term memory (LSTM) units
 
+from __future__ import print_function
+from keras.callbacks import LambdaCallback, ModelCheckpoint, EarlyStopping
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, LSTM, Bidirectional
 import numpy as np
 import sys
 import io
 import os
-from keras.models import Sequential
-from keras.callbacks import LambdaCallback, ModelCheckpoint, EarlyStopping
-from keras.layers import Dense, Dropout, Activation, LSTM, Bidirectional
 
-SEQUENCE_LEN = 3
+SEQUENCE_LEN = 7
 PERCENT_TEST = 10
 DROPOUT = 0
 BATCH_SIZE = 32
@@ -35,7 +36,7 @@ def sample(preds, temperature=1.0):
     preds = np.asarray(preds).astype('float64')
     preds = np.log(preds) / temperature
     expPreds = np.exp(preds)
-    preds = expPreds . np.sum(expPreds)
+    preds = expPreds / np.sum(expPreds)
     probas = np.random.multinomial(1, preds, 1)
     return np.argmax(probas)
 
@@ -43,7 +44,7 @@ def sample(preds, temperature=1.0):
 def onEpochEnd(epoch, logs):
     examplesFile.write('\n----- Generating text after Epoch: %d\n' % epoch)
 
-    seedIndex = np.random.randint(len(senTrain, senTest))
+    seedIndex = np.random.randint(len(senTrain+senTest))
     seed = (senTrain+senTest)[seedIndex]
 
     for diversity in [.3, .4, .5, .6, .7]:
@@ -77,44 +78,24 @@ if __name__ == "__main__":
         os.makedirs('./checkpoints/')
 
     # Making list of all words and lines in cleanQuotes.txt
-    # ('Quotes length in characters: ', 82099)
-    # ('Quotes length in words: ', 15975)
-    # ('Quotes length in lines: ', 944)
-
     with io.open(quotes) as f:
         # text is used to just count words and ignore new lines
-        text = f.read().replace('\n', ' ').encode('utf-8')
+        # text = f.read().replace('\n', ' ').encode('utf-8')
+                text = f.read().replace('\n', ' \n ').encode('utf-8')
 
     with io.open(quotes) as g:
         # textNewLine used to form sentences only within each quote.
-        textNewLine = g.read().replace('\n', ' \n').encode('utf-8')
-        # textNewLine = f.read().lower()
-
-    # print('Quotes length in characters: ', len(text))
-    # print(text)
-    # print(textNewLine)
+        textNewLine = g.read().encode('utf-8')
 
     textInWords = [w for w in text.split(' ') if w.strip() != '' or w == '\n']
     textInLines = [w.strip() for w in textNewLine.split('\n') if w.strip() != '']
 
-    # print(textInLines[0])
-    # print(type(textInLines[0]))
-
-    # for ww in textInLines:
-    #     print(ww)
-
-    # print('Quotes length in words: ', len(textInWords))
-    # print('Quotes length in lines: ', len(textInLines))
-    # print(textInLines[943])
-
     # Creating a set of textInWords to get unique words and then sorting set
     # then creating dictionaries of word to index and index to word
-    # ('Unique words in quotes: ', 2638)
+    textInWords = set(textInWords)
+    textInWords.add('')
+    words = sorted(textInWords)
 
-    words = sorted(set(textInWords))
-    # for ww in words:
-        # print(ww)
-    # print('Unique words in quotes: ', len(words))
     wordToIndex = dict((c,i) for i, c in enumerate(words))
     indexToWord = dict((i,c) for i, c in enumerate(words))
 
@@ -127,24 +108,21 @@ if __name__ == "__main__":
     # quotes in the sentence and nextWord. Once the nextWord is found on the
     # next line the loop should stop and start making sentences on the next
     # quote, this is to keep the net from generating random quotes
-
     sentences = []
     nextWords = []
     for currLine in textInLines:
         # currLine.encode('ascii')
         currLine = currLine.split(' ')
         for i in range(0, len(currLine)-SEQUENCE_LEN):
-            sentences.append(currLine[i: i + SEQUENCE_LEN])
-            nextWords.append(currLine[i+SEQUENCE_LEN])
+            if currLine[i+SEQUENCE_LEN] != '':
+                sentences.append(currLine[i: i + SEQUENCE_LEN])
+                nextWords.append(currLine[i+SEQUENCE_LEN])
 
-    # print(sentences[0])
-    # [str(x) for x in nextWords]
-    # for q in range(0, len(sentences)-1):
-    #     print(sentences[q], "~~~~~~", nextWords[q])
-        # print(type(nextWords[q]))
+        # sentences.append(currLine[len(currLine) - SEQUENCE_LEN: len(currLine)])
+        # nextWords.append('\n')
 
-    # print(type(nextWords[0]))
-
+    # print(sentences[:50])
+    # print(nextWords[:50])
     # Shuffle and split training and testing set
     tmpSentences = []
     tmpNextWord = []
@@ -156,19 +134,6 @@ if __name__ == "__main__":
     cutIndex = int(len(sentences) * (1.-(PERCENT_TEST/100.)))
     senTrain, senTest = tmpSentences[:cutIndex], tmpSentences[cutIndex:]
     nwTrain, nwTest = tmpNextWord[:cutIndex], tmpNextWord[cutIndex:]
-
-    # print("Length of sentences training set: ", len(senTrain))
-    # print("Length of nextWords training set: ", len(nwTrain))
-
-    # print("Length of sentences testing set: ", len(senTest))
-    # print("Length of nextWords testing set: ", len(nwTest))
-    # print('' in senTrain)
-    # print('' in nwTrain)
-    # print(nwTest)
-
-    # print('' in senTest)
-
-    # print('' in nwTest)
 
     # Training the model
     model = Sequential()
